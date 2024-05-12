e08a53291301f277e7950de196cee39576461fe8
Shagun Dubey <1202shagun@gmail.com>
2024-05-13 02:03:40 +0530

    update

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -1,7 +1,16 @@
-import { Badge } from '@nextui-org/react'
+import {
+  Badge,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+  User,
+} from '@nextui-org/react'
 import { SearchBar } from './components'
 import { BiSolidMessageSquareDetail } from 'react-icons/bi'
 import { IoNotifications } from 'react-icons/io5'
+import { FcDeleteRow } from 'react-icons/fc'
 
 export default function __header() {
   return (
@@ -15,6 +24,56 @@ export default function __header() {
         <Badge color="danger" content={50} shape="circle">
           <BiSolidMessageSquareDetail size={30} />
         </Badge>
+      </div>
+
+      <div>
+        <Dropdown
+          placement="bottom-start"
+          classNames={{ base: ['dark', 'w-[250px]'] }}
+        >
+          <DropdownTrigger>
+            <User
+              as="button"
+              avatarProps={{
+                isBordered: true,
+                src: 'https://i.pravatar.cc/150?u=a042581f4e29026024d',
+              }}
+              className="transition-transform"
+              description="@tonyreichert"
+              name="Tony Reichert"
+            />
+          </DropdownTrigger>
+          <DropdownMenu
+            variant="flat"
+            color="primary"
+            aria-label="Dropdown menu with description"
+            classNames={{ list: ['text-text'] }}
+            itemClasses={{
+              wrapper: ['w-[250px]', 'text-text'],
+            }}
+          >
+            <DropdownSection showDivider>
+              <DropdownItem className=" p-3">Overview</DropdownItem>
+              <DropdownItem className=" p-3">My Campaigns</DropdownItem>
+              <DropdownItem className=" p-3" startContent={''}>
+                My Organization
+              </DropdownItem>
+              <DropdownItem className=" p-3" startContent={''}>
+                Profile
+              </DropdownItem>
+            </DropdownSection>
+            <DropdownSection>
+              <DropdownItem
+                key="delete"
+                className="text-danger"
+                color="danger"
+                startContent={<FcDeleteRow />}
+              >
+                Log Out
+              </DropdownItem>
+            </DropdownSection>
+          </DropdownMenu>
+        </Dropdown>
       </div>
     </div>
   )
